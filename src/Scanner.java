740abf943486821902dd46bd8178f82aadf26ffe
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-05-29 05:13:38 +0900

    feat: Recognize Left/Right Bracket Token

--- src/Scanner.java
+++ src/Scanner.java
@@ -3,24 +3,22 @@ import java.io.*;
 public class Scanner {
 
     private boolean isEof = false;
-    private char ch = ' '; 
+    private char ch = ' ';
     private BufferedReader input;
     private String line = "";
     private int lineno = 0;
     private int col = 1;
     private final String letters = "abcdefghijklmnopqrstuvwxyz"
-        + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private final String digits = "0123456789";
     private final char eolnCh = '\n';
     private final char eofCh = '\004';
-    
 
-    public Scanner (String fileName) { // source filename
-    	System.out.println("Begin scanning... programs/" + fileName + "\n");
+    public Scanner(String fileName) { // source filename
+        System.out.println("Begin scanning... programs/" + fileName + "\n");
         try {
-            input = new BufferedReader (new FileReader(fileName));
-        }
-        catch (FileNotFoundException e) {
+            input = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
             System.out.println("File not found: " + fileName);
             System.exit(1);
         }
@@ -32,7 +30,7 @@ public class Scanner {
         col++;
         if (col >= line.length()) {
             try {
-                line = input.readLine( );
+                line = input.readLine();
             } catch (IOException e) {
                 System.err.println(e);
                 System.exit(1);
@@ -48,9 +46,8 @@ public class Scanner {
         } // if col
         return line.charAt(col);
     }
-            
 
-    public Token next( ) { // Return next token
+    public Token next() { // Return next token
         do {
             if (isLetter(ch) || ch == '_') { // ident or keyword
                 String spelling = concat(letters + digits + '_');
@@ -58,139 +55,163 @@ public class Scanner {
             } else if (isDigit(ch)) { // int literal
                 String number = concat(digits);
                 return Token.mkIntLiteral(number);
-            } else switch (ch) {
-            case ' ': case '\t': case '\r': case eolnCh:
-                ch = nextChar();
-                break;
-            
-            case '/':  // divide or divAssign or comment
-                ch = nextChar();
-                if (ch == '=')  { // divAssign
-                	ch = nextChar();
-                	return Token.divAssignTok;
-                }
-                
-                // divide
-                if (ch != '*' && ch != '/') return Token.divideTok;
-                
-                // multi line comment
-                if (ch == '*') { 
-    				do {
-    					while (ch != '*') ch = nextChar();
-    					ch = nextChar();
-    				} while (ch != '/');
-    				ch = nextChar();
-                }
-                // single line comment
-                else if (ch == '/')  {
-	                do {
-	                    ch = nextChar();
-	                } while (ch != eolnCh);
-	                ch = nextChar();
-                }
-                
-                break;
-            /*
-            case '\'':  // char literal
-                char ch1 = nextChar();
-                nextChar(); // get '
-                ch = nextChar();
-                return Token.mkCharLiteral("" + ch1);
-            */    
-            case eofCh: return Token.eofTok;
-            
-            case '+': 
-            	ch = nextChar();
-	            if (ch == '=')  { // addAssign
-	            	ch = nextChar();
-	            	return Token.addAssignTok;
-	            }
-	            else if (ch == '+')  { // increment
-	            	ch = nextChar();
-	            	return Token.incrementTok;
-	            }
-                return Token.plusTok;
+            } else
+                switch (ch) {
+                    case ' ':
+                    case '\t':
+                    case '\r':
+                    case eolnCh:
+                        ch = nextChar();
+                        break;
 
-            case '-': 
-            	ch = nextChar();
-                if (ch == '=')  { // subAssign
-                	ch = nextChar();
-                	return Token.subAssignTok;
-                }
-	            else if (ch == '-')  { // decrement
-	            	ch = nextChar();
-	            	return Token.decrementTok;
-	            }
-                return Token.minusTok;
+                    case '/': // divide or divAssign or comment
+                        ch = nextChar();
+                        if (ch == '=') { // divAssign
+                            ch = nextChar();
+                            return Token.divAssignTok;
+                        }
 
-            case '*': 
-            	ch = nextChar();
-                if (ch == '=')  { // multAssign
-                	ch = nextChar();
-                	return Token.multAssignTok;
-                }
-                return Token.multiplyTok;
+                        // divide
+                        if (ch != '*' && ch != '/')
+                            return Token.divideTok;
 
-            case '%': 
-            	ch = nextChar();
-                if (ch == '=')  { // remAssign
-                	ch = nextChar();
-                	return Token.remAssignTok;
-                }
-                return Token.reminderTok;
+                        // multi line comment
+                        if (ch == '*') {
+                            do {
+                                while (ch != '*')
+                                    ch = nextChar();
+                                ch = nextChar();
+                            } while (ch != '/');
+                            ch = nextChar();
+                        }
+                        // single line comment
+                        else if (ch == '/') {
+                            do {
+                                ch = nextChar();
+                            } while (ch != eolnCh);
+                            ch = nextChar();
+                        }
 
-            case '(': ch = nextChar();
-            return Token.leftParenTok;
+                        break;
+                    /*
+                     * case '\'': // char literal
+                     * char ch1 = nextChar();
+                     * nextChar(); // get '
+                     * ch = nextChar();
+                     * return Token.mkCharLiteral("" + ch1);
+                     */
+                    case eofCh:
+                        return Token.eofTok;
 
-            case ')': ch = nextChar();
-            return Token.rightParenTok;
+                    case '+':
+                        ch = nextChar();
+                        if (ch == '=') { // addAssign
+                            ch = nextChar();
+                            return Token.addAssignTok;
+                        } else if (ch == '+') { // increment
+                            ch = nextChar();
+                            return Token.incrementTok;
+                        }
+                        return Token.plusTok;
 
-            case '{': ch = nextChar();
-            return Token.leftBraceTok;
+                    case '-':
+                        ch = nextChar();
+                        if (ch == '=') { // subAssign
+                            ch = nextChar();
+                            return Token.subAssignTok;
+                        } else if (ch == '-') { // decrement
+                            ch = nextChar();
+                            return Token.decrementTok;
+                        }
+                        return Token.minusTok;
 
-            case '}': ch = nextChar();
-            return Token.rightBraceTok;
+                    case '*':
+                        ch = nextChar();
+                        if (ch == '=') { // multAssign
+                            ch = nextChar();
+                            return Token.multAssignTok;
+                        }
+                        return Token.multiplyTok;
 
-            case ';': ch = nextChar();
-            return Token.semicolonTok;
+                    case '%':
+                        ch = nextChar();
+                        if (ch == '=') { // remAssign
+                            ch = nextChar();
+                            return Token.remAssignTok;
+                        }
+                        return Token.reminderTok;
 
-            case ',': ch = nextChar();
-            return Token.commaTok;
-                
-            case '&': check('&'); return Token.andTok;
-            case '|': check('|'); return Token.orTok;
+                    case '(':
+                        ch = nextChar();
+                        return Token.leftParenTok;
 
-            case '=':
-                return chkOpt('=', Token.assignTok,
-                                   Token.eqeqTok);
+                    case ')':
+                        ch = nextChar();
+                        return Token.rightParenTok;
 
-            case '<':
-                return chkOpt('=', Token.ltTok,
-                                   Token.lteqTok);
-            case '>': 
-                return chkOpt('=', Token.gtTok,
-                                   Token.gteqTok);
-            case '!':
-                return chkOpt('=', Token.notTok,
-                                   Token.noteqTok);
+                    case '{':
+                        ch = nextChar();
+                        return Token.leftBraceTok;
 
-            default:  error("Illegal character " + ch); 
-            } // switch
+                    case '}':
+                        ch = nextChar();
+                        return Token.rightBraceTok;
+
+                    // Left Bracket Token Add - 추가 구현
+                    case '[':
+                        ch = nextChar();
+                        return Token.leftBracketTok;
+                    // Right Bracket Token Add - 추가 구현
+                    case ']':
+                        ch = nextChar();
+                        return Token.rightBracketTok;
+
+                    case ';':
+                        ch = nextChar();
+                        return Token.semicolonTok;
+
+                    case ',':
+                        ch = nextChar();
+                        return Token.commaTok;
+
+                    case '&':
+                        check('&');
+                        return Token.andTok;
+                    case '|':
+                        check('|');
+                        return Token.orTok;
+
+                    case '=':
+                        return chkOpt('=', Token.assignTok,
+                                Token.eqeqTok);
+
+                    case '<':
+                        return chkOpt('=', Token.ltTok,
+                                Token.lteqTok);
+                    case '>':
+                        return chkOpt('=', Token.gtTok,
+                                Token.gteqTok);
+                    case '!':
+                        return chkOpt('=', Token.notTok,
+                                Token.noteqTok);
+
+                    default:
+                        error("Illegal character " + ch);
+                } // switch
         } while (true);
     } // next
 
-
     private boolean isLetter(char c) {
-        return (c>='a' && c<='z' || c>='A' && c<='Z');
+        return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
     }
-  
+
     private boolean isDigit(char c) {
-        return (c>='0' && c<='9');
+        return (c >= '0' && c <= '9');
     }
 
     private void check(char c) {
         ch = nextChar();
-        if (ch != c) 
+        if (ch != c)
             error("Illegal character, expecting " + c);
         ch = nextChar();
     }
@@ -212,18 +233,18 @@ public class Scanner {
         return r;
     }
 
-    public void error (String msg) {
+    public void error(String msg) {
         System.err.print(line);
         System.err.println("Error: column " + col + " " + msg);
         System.exit(1);
     }
 
-    static public void main ( String[] argv ) {
+    static public void main(String[] argv) {
         Scanner lexer = new Scanner(argv[0]);
-        Token tok = lexer.next( );
+        Token tok = lexer.next();
         while (tok != Token.eofTok) {
             System.out.println(tok.toString());
-            tok = lexer.next( );
-        } 
+            tok = lexer.next();
+        }
     } // main
 }
